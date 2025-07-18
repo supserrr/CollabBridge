5654199eb3d7522f8ebad9236c5e428c608cee32
supserrr <s.shima@alustudent.com>
2025-07-18 19:19:51 +0200

    removed front end

--- frontend/src/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Input } from './Input';
-export { Textarea } from './Textarea';
-export { Select } from './Select';
-export { Checkbox } from './Checkbox';
-export { Button } from './Button';
-export { FileUpload } from './FileUpload';
-export { FormField } from './FormField';
-export { MultiStepForm } from './MultiStepForm';
